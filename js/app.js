05494bd4bb9ad2e72974b8d0f25d118001a6b207
lijiarui <ruiruibupt@126.com>
2015-05-28 20:06:29 +0800

    20150528

--- js/app.js
+++ js/app.js
@@ -7,11 +7,12 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', [
   'ionic',
+  // 'ui.router',
   'ngCordova',
   'ionic.service.core',
   'ionic.service.push',
   'ionic.service.deploy',
-  'starter.controllers'
+  'starter.controllers',
 ])
 
 .config(['$ionicAppProvider', function($ionicAppProvider) {
@@ -26,32 +27,151 @@ angular.module('starter', [
   });
 }])
 
-.run(function($rootScope, $ionicDeploy, $ionicPlatform, $cordovaStatusbar) {
+.config(function($ionicConfigProvider){
+  console.log(">>>>>>>");
+  console.log($ionicConfigProvider);
+  // $ionicConfigProvider.backButton.previousTitleText(false).text('');
+})
 
+.factory("Format",function(){
+
+  function formatcell (cell){
+    if(cell == 1){
+            return 1;
+          }
+          if(cell == 2){
+            return 2;
+          }
+          else{
+            return 3;
+          }
+  };
+
+  return {
+    formatcell:formatcell
+  }
+
+})
+
+.factory("ContactService", function($http,$ionicLoading,$q){
+
+  var service = {};
+
+  service.getMainInfo = function(){
+    return $http.get('data/contact.json');
+  }
+
+  return service;
+})
+
+.factory("MessageService", function($http,$ionicLoading,$q){
+
+  var service = {};
+
+  service.getMainInfo = function(){
+    return $http.get('data/message.json');
+  }
+
+  return service;
+})
+
+.factory("PersonalHomepageService", function($http,$ionicLoading,$q){
+
+  var service = {};
+
+  service.getUserInfo = function(){
+    return $http.get('data/friendcircle_account.json');
+  };
+
+  service.getContentInfo = function(){
+    return $http.get('data/personalHomepage.json');
+  };
+
+  return service;
+})
+
+.factory("AuthService",function($http,$location,$window){
+//	var authService = {};
+	function login (username,password){
+		return $http
+//		.post('http://17salsa.com/login.php',{username:"zixia"})
+		.post('http://127.0.0.1/test.php',{username:username})
+		.then(function(res){
+      if (res.data.ret == true) {
+        $window.sessionStorage['sessionId'] = res.data.sid;
+        $window.sessionStorage['userId'] = res.data.userid;
+        $window.sessionStorage['userName'] = res.data.username;
+        $window.sessionStorage['ret'] = res.data.ret;
+        alert('登陆成功');
+        $location.path('/tab/home');
+      }
+      else{
+        console.log('login error');
+        alert('用户名密码错误！');
+      }
+			return res.data;
+		});
+	};
+	
+	function isAuthenticated (){
+    return $window.sessionStorage['userId'];
+    // return true;
+  }
+	// authService.isAuthenticated = function(){
+	// 	return !!Session.userId;
+	// };
+
+  function logout(){
+    // $window.sessionStorage.clear();//清空所有内容，我还是不用了把 = =
+    $window.sessionStorage['sessionId'] = null;
+    $window.sessionStorage['userId'] = null;
+    $window.sessionStorage['userName'] = null;
+    $window.sessionStorage['ret'] = null;
+    console.log('logout successful');
+  }
+
+	return {
+		login: login,
+    logout:logout,
+    isAuthenticated:isAuthenticated
+		}
+})
+	
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-
-    // Hide the accessory bar by default
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-
-    // Color the iOS status bar text to white
-    if (window.StatusBar) {
-      $cordovaStatusbar.overlaysWebView(true);
-      $cordovaStatusBar.style(1); //Light
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
     }
-
-    // Default update checking
-    $rootScope.updateOptions = {
-      interval: 2 * 60 * 1000
-    };
-
-    // Watch Ionic Deploy service for new code
-    $ionicDeploy.watch($rootScope.updateOptions).then(function() {}, function() {}, function(hasUpdate) {
-      $rootScope.lastChecked = new Date();
-      console.log('WATCH RESULT', hasUpdate);
-    });
   });
+})
+
+.run(function($rootScope, $location, AuthService,$state) {
+  // console.log(arguments);
+
+  $rootScope.$on("$stateChangeStart",function(event,a,b,c,d,e){
+    // console.log("???");
+    // console.log(AuthService);
+    // if (AuthService.isAuthenticated == "null") {
+    //   console.log('s');
+    //   $state.go("tab.user");//从user那里跳过来还是有问题。
+    //   // $location.path("/tab/user");//uiroute和ngroute的区别 uiroute是angularjs的扩展
+    // };
+  })
+
+  $rootScope.$on("$stateChangeError",function(event,a,b,c,d,e){
+    event.preventDefault(); //Note: Use event.preventDefault() to prevent the transition from happening.
+    // console.log(arguments);
+    // console.log(e);
+    if (e.authenticated === false) {
+      $state.go("tab.user");//从user那里跳过来还是有问题。
+      // $location.path("/tab/user");//uiroute和ngroute的区别 uiroute是angularjs的扩展
+    };
+  })
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -69,28 +189,116 @@ angular.module('starter', [
     templateUrl: "templates/tabs.html"
   })
 
-  // Each tab has its own nav history stack:
+  //用户登录
+  .state('login', {
+    url: '/login',
+    controller: 'LoginCtrl',
+    templateUrl: 'templates/tab-login.html',
+  })
 
-  // Welcome tab
-  .state('tab.home', {
-    url: '/home',
+  .state('tab.activity', {
+    url: '/activity',
     views: {
-      'tab-home': {
-        templateUrl: 'templates/tab-home.html',
-        controller: 'HomeCtrl'
+      'tab-activity': {
+        templateUrl: 'templates/tab-activity.html',
+        controller: 'ActivityCtrl'
       }
     }
   })
-  
-  // 朋友圈 tab
-  .state('tab.friendcircle', {
-    url: '/friendcircle',
+
+  .state('tab.message', {
+    url: '/message',
     views: {
-      'tab-friendcircle': {
-        templateUrl: 'templates/tab-friendcircle.html',
-        controller: 'FriendcircleCtrl'
+      'tab-message': {
+        templateUrl: 'templates/tab-message.html',
+        controller: 'MessageCtrl'
       }
     }
+  })
+
+  .state('message-detail', {
+      url: '/message/:messageId',
+          templateUrl: 'templates/message-detail.html',
+          controller: 'MessageDetailCtrl'
+  })
+
+  .state('contact', {
+    url: '/contact',
+    // views: {
+    //   'tab-contact': {
+        templateUrl: 'templates/tab-contact.html',
+        controller: 'ContactCtrl'
+    //   }
+    // }
+  })
+
+  .state('contact-detail', {
+      url: '/contact/:contactId',
+          templateUrl: 'templates/contact-detail.html',
+          controller: 'ContactDetailCtrl'
+  })
+
+  .state('tab.discovery', {
+    url: '/discovery',
+    views: {
+      'tab-discovery': {
+        templateUrl: 'templates/tab-discovery.html',
+        controller: 'DiscoveryCtrl'
+      }
+    }
+  })
+
+  // .state('tab.discovery-test', {
+  //   url: '/discovery/11111111111111111',
+  //   views: {
+  //     'tab-discovery': {
+  //       templateUrl: 'templates/test.html',
+  //       controller: 'TestCtrl'
+  //     }
+  //   }
+  // })
+  
+  .state('friendcircle', {
+    url: '/friendcircle',
+      // views{
+      //   'tab.discovery-friendcircle':{
+        templateUrl: 'templates/tab-friendcircle.html',
+        controller: 'FriendcircleCtrl',
+        // resolve:{
+        //   auth:function($q,AuthService){
+        //     var flag = AuthService.isAuthenticated();
+        //     console.log(flag);
+        //     console.log(AuthService);
+        //     if (flag!='null') {
+        //       console.log("ruiruiruiruir");
+        //       return $q.when(flag);
+        //     }
+        //     else{
+        //       console.log('exit');
+        //       // $scope.$emit('authenticated','false');
+        //       // console.log($q);
+        //       // console.log($q.defer());
+        //       return $q.reject({ authenticated: false });
+        //     }
+
+////        	auth:["$q","authenticationSvc",function($q,authenticationSvc){
+////        		console.log(authenticationSvc);
+////        		var userInfo = authenticationSvc.getUserInfo();
+////        		console.log("authentication里面的userInfo:"+userInfo);
+////        		if(userInfo){
+////        			console.log('when');
+////        			return $q.when(userInfo);
+////        		}
+////        		else{
+////        			console.log('reject');
+////        			console.log($q.reject({authenticated:false}));
+////        			return $q.reject({authenticated:false});
+////        		}
+////        	}]
+//        }
+    //   }
+    // }
+    // }}
   })
   
   // 聊天室 tab
@@ -104,38 +312,22 @@ angular.module('starter', [
     }
   })
 
-  // Ionic User tab
-  .state('tab.user', {
-    url: '/user',
+  .state('tab.account', {
+    url: '/account',
     views: {
-      'tab-user': {
-        templateUrl: 'templates/tab-user.html',
-        controller: 'UserCtrl'
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
       }
     }
   })
 
-  // Ionic Push tab
-  .state('tab.push', {
-    url: '/push',
-    views: {
-      'tab-push': {
-        templateUrl: 'templates/tab-push.html',
-        controller: 'PushCtrl'
-      }
-    }
+  .state('personalHomepage', {
+      url: '/personalHomepage',
+          templateUrl: 'templates/personal_homepage.html',
+          controller: 'PersonalHomepageCtrl'
   })
 
-  // Ionic Deploy tab
-  .state('tab.deploy', {
-    url: '/deploy',
-    views: {
-      'tab-deploy': {
-        templateUrl: 'templates/tab-deploy.html',
-        controller: 'DeployCtrl'
-      }
-    }
-  })
 
   // Ionic Analytics tab
   .state('tab.analytics', {
@@ -149,6 +341,6 @@ angular.module('starter', [
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
+  $urlRouterProvider.otherwise('/tab/message');
 
 });
--- js/controllers.js
+++ js/controllers.js
@@ -1,210 +1,324 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope, $rootScope, $ionicPush, $ionicUser) {
+.controller('ActivityCtrl', function($http, $scope,$ionicLoading) {
+    // $ionicLoading.show({
+    //     template:'<i class = "ion-load-c"><br></i>Loading...'
+    // });
+
+    // $http.get('data/contact.json').success(function(data) {
+    //       $scope.contacts = data;
+    //     }).then(function(){
+    //         $ionicLoading.hide();
+    // });
+
+})
+
+// .controller("TestCtrl",function($http,$scope){
+//     console.log("test");
+// })
+
+.controller('MessageCtrl', function($http, $scope,$ionicLoading,MessageService) {
+    $ionicLoading.show({
+        template:'<i class = "ion-load-c"><br></i>Loading...'
+    });
+
+    MessageService.getMainInfo().success(function(data){
+        $scope.messages = data;
+        }).then(function(){
+            $ionicLoading.hide();
+    });
+})
+
+.controller('MessageDetailCtrl', function($scope,$stateParams,$state,$ionicLoading,MessageService)  {
+
+    var num = $stateParams.messageId;
+
+    var account_img_src = "img/con4.jpg";
+
+    $ionicLoading.show({
+        template:'<i class = "ion-load-c"><br></i>Loading...'
+    });
+
+    MessageService.getMainInfo().success(function(data) {
+          for (var i = 0; i < data.length; i++) {
+            if(data[i].message_user_id == num){
+                $scope.messageitem = data[i];
+            }
+        }
+    }).then(function(){
+        $ionicLoading.hide();
+    });
+
+    $scope.gomessage = function(){
+            $state.go("tab.message");
+    }
+
+    $scope.format_img = function(source,img_src){
+        if (source == "right") {
+            return account_img_src; 
+        }
+        else{
+            return img_src;
+        }
+    }
+
+    //需要和紫霞调
+    $scope.sendmessagedetail = function(){
+        $message_content = $scope.message_detail_send;
+        $message_json = {"name":"right","content":$message_content};
+        $scope.messageitem.message_array.push($message_json);
+    }
+})
+
+.controller('ContactCtrl', function($http, $scope,$ionicLoading,$state,ContactService) {
+    $ionicLoading.show({
+        template:'<i class = "ion-load-c"><br></i>Loading...'
+    });
+
+    ContactService.getMainInfo().success(function(data){
+        $scope.contacts = data;
+    }).then(function(){
+            $ionicLoading.hide();
+    });
+
+    $scope.godiscover = function(){
+            $state.go("tab.discovery");
+    }
+
+})
+
+.controller('ContactDetailCtrl', function($scope,$stateParams,$state,$ionicLoading,ContactService) {
+
+    var num = $stateParams.contactId;
+
+    $ionicLoading.show({
+        template:'<i class = "ion-load-c"><br></i>Loading...'
+    });
+
+    ContactService.getMainInfo().success(function(data){
+        for (var i = 0; i < data.length; i++) {
+            if(data[i].contact_id == num){
+                $scope.contactitem = data[i];
+            }
+        }
+    }).then(function(){
+            $ionicLoading.hide();
+    });
+
+    $scope.gocontact = function(){
+            $state.go("contact");
+    }
+
+    // $scope.sendmessage = function(contact_id){
+    //     $state.go("tab.message",{contact_id:contact_id});
+    // }
+
+})
+
+.controller('DiscoveryCtrl', function($scope,$state) {
+    $scope.gofriendcircle = function(){
+        $state.go("friendcircle");
+    }
+
+    $scope.gocontact = function(){
+        $state.go("contact");
+    }
+})
+
+.controller('ChatroomCtrl', function($scope) {
   // Nothing to see here.
 })
 
-.controller('ChatroomCtrl', function($scope, $rootScope, $ionicPush, $ionicUser) {
-  // Nothing to see here.
+.controller('FriendcircleHeaderCtrl',function($scope){
+    // $http.get('http://17salsa.com/home/s.php?rewrite=home-view-all').success(function(data) {
+    //       $scope.infos = data;
+    //     });
 })
 
-.controller('FriendcircleCtrl', function($scope,$http,$ionicPopup) {
+.controller('FriendcircleCtrl', function($scope,$http,$ionicPopup,Format,$ionicLoading,$state,$rootScope) {       
+        $scope.clickarray = new Array();
+        $scope.friend_id = null;
+        $scope.inputshow = false;
 
-//        $http.get('data/friendcircle.json').success(function(data) {
-		 $http.get('http://17salsa.com/home/s.php?rewrite=home-view-all').success(function(data) {
+
+        $scope.godiscover = function(){
+            $state.go("tab.discovery");
+        }
+
+        $scope.clearclick = function(){
+            $scope.clickarray = new Array();
+            $scope.inputshow = false;
+        }
+
+        $scope.clickfun = function(num){
+            var user = "aaaaaaa";
+
+            if($scope.infos[num].reply_heart.indexOf(user)>-1){
+                $scope.heart_tag =  "取消";
+            }
+            else{
+                $scope.heart_tag =  "点赞";
+            }   
+
+            $scope.clickarray[num] = !$scope.clickarray[num];
+            $scope.serial_num = num;
+
+            
+        }
+
+        $scope.searchclick = function(num){
+            return $scope.clickarray[num];
+        }
+
+        $scope.remark = function(id){
+            $scope.friend_id = id;
+            $scope.inputshow = true;
+            console.log("id:"+ id);
+        }
+
+        //发送评论 和zixia调
+        $scope.sendremark = function(){
+            $remark_content = $scope.inputContent;
+            $remark_json = {"name":"newremark","content":$remark_content};
+            var serial = $scope.serial_num;//整个数据流中的第几个数据
+            $scope.infos[serial].reply.push($remark_json);
+            // return $http
+            // // .post('http://17salsa.com/login.php',{username:"zixia"})
+            // .post('http://127.0.0.1/test.php',{
+            //     content:content,
+            //     friend_id:$scope.friend_id
+            // }).then(function(res){
+            //   if (res.data.ret == true) {
+                // $scope.infos[serial].reply.push($remark_json);
+
+            //   }
+            //   else{
+            //     console.log('aaaaaaa');
+            //     alert('评论失败');
+            //   }
+            // return res.data;
+            // };
+        }
+
+        //点赞 和zixia调
+        $scope.sendheart = function(){
+            var user = 'aaaaaaa';
+            var serial = $scope.serial_num;//整个数据流中的第几个数据
+            if($scope.infos[serial].reply_heart.indexOf(user)>-1){
+                var reply_heart_index = $scope.infos[serial].reply_heart.indexOf(user);
+                $scope.infos[serial].reply_heart.splice(reply_heart_index, 1);
+            }
+            else{
+                $scope.infos[serial].reply_heart.push(user);
+            }   
+        }
+
+         $ionicLoading.show({
+            template:'<i class = "ion-load-c"><br></i>Loading...'
+         });
+         $http.get('data/friendcircle.json').success(function(data) {
+         // $http.get('http://17salsa.com/home/s.php?rewrite=home-view-all').success(function(data) {
           $scope.infos = data;
+        }).then(function(){
+            $ionicLoading.hide();
         });
         
         //格式化类,根据收到的图片展示不同的样式
         $scope.formatcell = function(cell){
-        	if(cell == 1){
-        		return 1;
-        	}
-        	if(cell == 2){
-        		return 2;
-        	}
-        	else
-        		return 3;
+            return Format.formatcell(cell);
         }
-        
-        $scope.showPopup = function(){
-        	var myPopup = $ionicPopup.show({
-        		templateUrl:"../templates/input.html",
-        		scope:$scope,
-        		buttons:[
-        		   {text:'取消'},
-        		   {
-        			text:'保存',
-        			type:'button-positive',
-        		   }
-        		]
-        	});
+
+        $scope.refresh = function(){
+            $http.get('http://17salsa.com/home/s.php?rewrite=home-view-all').success(function(data) {
+              $scope.infos = data;
+            }).then(function(){
+                $scope.$broadcast('scroll.refreshComplete');
+            });
         }
+
+        
+        
+
+
+       
+        
+        // $scope.showPopup = function(){
+        // 	var myPopup = $ionicPopup.show({
+        // 		templateUrl:"../templates/input.html",
+        // 		scope:$scope,
+        // 		buttons:[
+        // 		   {text:'取消'},
+        // 		   {
+        // 			text:'保存',
+        // 			type:'button-positive',
+        // 		   }
+        // 		]
+        // 	});
+        // }
         
         
 })
 
+.controller('AccountCtrl', function($http, $scope,$ionicLoading,$ionicNavBarDelegate) {
+	$ionicNavBarDelegate.showBackButton(true);
 
-.controller('UserCtrl', function($scope, $rootScope, $ionicUser) {
-//  /**
-//   * Identifies a new user with the Ionic User service (read the docs at http://docs.ionic.io/identify/). This should be
-//   * called before any other registrations take place.
-//   **/
-//  $scope.identifyUser = function() {
-//    console.log('Ionic User: Identifying with Ionic User service');
-//
-//    var user = $ionicUser.get();
-//    if(!user.user_id) {
-//      // Set your user_id here, or generate a random one.
-//      user.user_id = $ionicUser.generateGUID()
-//    };
-//
-//    // Add some metadata to your user object.
-//    angular.extend(user, {
-//      name: 'Ionitron',
-//      message: 'I come from planet Ion'
-//    });
-//
-//    // Identify your user with the Ionic User Service
-//    $ionicUser.identify(user).then(function(){
-//      alert('Successfully identified user ' + user.name + '\n ID ' + user.user_id);
-//    });
-//  };
+    $ionicLoading.show({
+        template:'<i class = "ion-load-c"><br></i>Loading...'
+    });
+
+    $http.get('data/user.json').success(function(data) {
+          $scope.users = data;
+        }).then(function(){
+            $ionicLoading.hide();
+    });
 })
 
-.controller('PushCtrl', function($http, $scope, $rootScope, $ionicPush, $ionicApp) {
-//  // Put your private API key here to be able to send push notifications from within the app.
-//  // TODO: Add your private API key here if you want to push from your device.
-//  $scope.privateKey = '';
-//
-//  // Write your own code here to handle new device tokens from push notification registration as they come in.
-//  $rootScope.$on('$cordovaPush:tokenReceived', function(event, data) {
-//    alert("Successfully registered token " + data.token);
-//    console.log('Ionic Push: Got token ', data.token, data.platform);
-//    $scope.token = data.token;
-//  });
-//
-//  /**
-//   * Registers the currently identified Ionic User for push notifications on the current device. This should either pass
-//   * a user object to identify or be called after $ionicUser.identify()
-//   * (read the docs at http://docs.ionic.io/push/installation/).
-//   **/
-//  $scope.pushRegister = function() {
-//    console.log('Ionic Push: Registering user');
-//
-//    // Register with the Ionic Push service.  All parameters are optional.
-//    $ionicPush.register({
-//      canShowAlert: true, //Should new pushes show an alert on your screen?
-//      canSetBadge: true, //Should new pushes be allowed to update app icon badges?
-//      canPlaySound: true, //Should notifications be allowed to play a sound?
-//      canRunActionsOnWake: true, // Whether to run auto actions outside the app,
-//      onNotification: function(notification) {
-//        // Handle new push notifications here
-//        // console.log(notification);
-//        return true;
-//      }
-//    }).then(function(deviceToken) {
-//      //Save the device token, if necessary
-//    });
-//  };
-//
-//  /**
-//   * If you've added your Private API Key, you can send a push notification directly fro the current device.  Since the
-//   * app iwll be open when this happens, you probably will not see the notification handled by the OS, but it should
-//   * still be handled by whatever function you define.
-//   **/
-//  $scope.sendPush = function() {
-//    if ($scope.privateKey) {
-//      alert('A notification will be sent to you 5 seconds after you close this alert.  They can take a few minutes to arrive.');
-//      var appId = $ionicApp.getApp().app_id;
-//      var auth = btoa($scope.privateKey + ':'); // Base64 encode your key
-//      var req = {
-//        method: 'POST',
-//        url: 'https://push.ionic.io/api/v1/push',
-//        headers: {
-//          'Content-Type': 'application/json',
-//          'X-Ionic-Application-Id': appId,
-//          'Authorization': 'basic ' + auth
-//        },
-//        data: {
-//          "tokens": [$scope.token],
-//          "notification": {
-//            "alert":"Hello World!"
-//          }
-//        }
-//      };
-//
-//      setTimeout(function(){
-//        $http(req).success(function(resp){
-//            console.log("Ionic Push: Push success!");
-//          }).error(function(error){
-//            console.log("Ionic Push: Push error...");
-//          });
-//      }, 5000);
-//    } else {
-//      alert('Uh-oh!  To use this function, add your Private API Key to line 36 of controllers.js');
-//    }
-//  };
+
+.controller('PersonalHomepageCtrl', function($http, $scope,$ionicLoading,PersonalHomepageService) {
+    $ionicLoading.show({
+        template:'<i class = "ion-load-c"><br></i>Loading...'
+    });
+
+    PersonalHomepageService.getUserInfo().success(function(data) {
+        $scope.userBasicInfo = data;
+    });
+
+    PersonalHomepageService.getContentInfo().success(function(data) {
+        $scope.userContentInfo = data;
+    }).then(function(){
+        $ionicLoading.hide();
+    });
+    
 })
 
-.controller('AnalyticsCtrl', function($scope, $ionicAnalytics) {
-//
-//  // Track a fake purchase event.
-//  $scope.trackPurchase = function() {
-//    console.log("Ionic Analytics: Tracking a fake purchase.");
-//    $ionicAnalytics.track('purchase', {
-//      item_id: 101,
-//      item_name: 'A-Trak player'
-//    });
-//    alert('Tracked purchase of A_Trak player ID 101.');
-//  };
-//
-//  // Track a fake review event
-//  $scope.trackReview = function() {
-//    console.log("Ionic Analytics: Tracking a fake review.");
-//    $ionicAnalytics.track('review', {
-//      star_rating: 5,
-//      reviewer_name: 'John',
-//      content: 'Awesome app!'
-//    });
-//    alert('Tracked 5-star review from John, "Awesome app!"');
-//  };
+.controller('LoginCtrl',function($scope,$rootScope,AuthService){
+    $scope.login = function(username,password){
+         AuthService.login(username,password)
+        .then(function(res){
+            if (res.ret === true) {
+                console.log(res);
+                $rootScope.$broadcast(res);
+            }
+            else{
+                $rootScope.$broadcast("login failed");
+            }
+            
+        },function(){
+            $rootScope.$broadcast("transimit failed wuwuwu");
+        });
+    };
+
+    $scope.logout = function(){
+        AuthService.logout();
+    }
+
+    javascript:void(0);
 })
 
-.controller('DeployCtrl', function($scope, $rootScope, $ionicDeploy) {
-//  $scope.updateMinutes = 2;
-//
-//  // Handle action when update is available
-//  $rootScope.$on('$ionicDeploy:updateAvailable', function() {
-//    console.log('Ionic Deploy: New update available!');
-//    $scope.hasUpdate = true;
-//  });
-//
-//  // Stop checking for updates form Ionic Deploy
-//  $scope.stopCheckingForUpdates = function() {
-//    $ionicDeploy.unwatch();
-//  };
-//
-//  // Update app code with new release from Ionic Deploy
-//  $scope.doUpdate = function() {
-//    $ionicDeploy.update().then(function(res) {
-//      console.log('Ionic Deploy: Update Success! ', res);
-//    }, function(err) {
-//      console.log('Ionic Deploy: Update error! ', err);
-//    }, function(prog) {
-//      console.log('Ionic Deploy: Progress... ', prog);
-//    });
-//  };
-//
-//  // Check Ionic Deploy for new code
-//  $scope.checkForUpdates = function() {
-//    console.log('Ionic Deploy: Checking for updates');
-//    $ionicDeploy.check().then(function(hasUpdate) {
-//      console.log('Ionic Deploy: Update available: ' + hasUpdate);
-//      $rootScope.lastChecked = new Date();
-//      $scope.hasUpdate = hasUpdate;
-//    }, function(err) {
-//      console.error('Ionic Deploy: Unable to check for updates', err);
-//    });
-//  }
-});
+
+
+.controller('AnalyticsCtrl', function($scope) {
+
+})
+
+
