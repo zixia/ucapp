ca6de7467fe4fe45660d169f4d1baec71052e5f9
zixia <zixia@zixia.net>
2015-06-04 17:02:35 +0800

    #4 add login status check.

--- ucapi/showmessage.php
+++ ucapi/showmessage.php
@@ -11,8 +11,10 @@ $res = file_get_contents('php://input');
 $data = json_decode($res,true);//生成array数组
 
 $response = showmessage1($data);
+
 $response_json = json_encode($response);//生成json数据
-print_r($response_json);
+die($response_json);
+
 
 /**
  * @Author:      ruirui
@@ -30,11 +32,20 @@ print_r($response_json);
  * 			     $response[]['message_array'][]['content']	string	发送内容	
  */
 
+/*
+define('RET_ERR_UNKNOWN', -1);
+define('RET_ERR_NOLOGIN', 1);
+*/
+
 function showmessage1($data) {
     global $_SGLOBAL;
 
-    $user_id = $data['user_id'];
 	$response = array();
+    $response['ret'] = false;
+
+    if(!$_SGLOBAL['supe_uid']) { // need login
+        return $response;
+    }
 
     $list = array();
 
@@ -69,8 +80,10 @@ function showmessage1($data) {
         $page = empty($_GET['page'])?0:intval($_GET['page']);
         if($page<1) $page = 1;
 
-        $result = uc_pm_list($_SGLOBAL['supe_uid'], $page, $perpage, 'inbox', $filter, 1000);
+        $result = uc_pm_list($_SGLOBAL['supe_uid'], $page, $perpage, 'inbox', $filter, 100);
 
+//die("$_SGLOBAL[supe_uid], $page, $perpage, 'inbox', $filter, 100");
+//print_r($result);
         $count = $result['count'];
         $list = $result['data'];
 
