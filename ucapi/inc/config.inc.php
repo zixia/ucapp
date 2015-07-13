ac803ca2cbf1a3af2ad266329904b7611cc717ef
zixia <zixia@zixia.net>
2015-07-13 19:44:27 +0800

    minor changes

--- ucapi/inc/config.inc.php
+++ ucapi/inc/config.inc.php
@@ -1,16 +1,18 @@
 <?php
+header("Content-Type:text/html; charset=utf-8");
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+$origin = '*' ;
 if ( function_exists('apache_request_headers') ) {
     $req_headers = apache_request_headers();
+    $origin = $req_headers['Origin'];
 }
-$origin = isset($req_headers['Origin']) ? $req_headers['Origin'] : '*' ;
 
 // allow ajax pass cookie
 header("Access-Control-Allow-Origin: $origin");
 header('Access-Control-Allow-Credentials: true');
 
-header("Content-Type:text/html; charset=utf-8");
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 include_once('/750/xfs/vhost/17salsa.com/home/common.php');
 include_once(S_ROOT.'./source/function_cp.php');
