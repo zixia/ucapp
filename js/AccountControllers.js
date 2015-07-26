f001254799e7ede3f3bed4eb50223f854f6199b9
LI Zhuohuan <zixia@zixia.net>
2015-07-26 15:20:22 +0800

    variable name shoule follow swift rules. (javascript has too many styles)

--- js/AccountControllers.js
+++ js/AccountControllers.js
@@ -20,43 +20,33 @@ angular.module('starter.accountcontrollers', [])
 
 
 
-.controller('LoginCtrl',function($scope,$rootScope,AuthService,$ionicPopup){
+.controller('LoginCtrl',function($scope,$rootScope,AuthService,$ionicPopup,$log){
     $scope.checklogin = false;
-    $scope.err_txt = '';
+    $scope.errTxt = '';
     $scope.login = function(username,password){
-         AuthService.login(username,password)
+        $log.log("logining, username: " + username );
+        AuthService.login(username,password)
         .then(function(res){
+            $log.log('AuthService.login.then...' );
             if (res.ret === true) {
                 //console.log("resresres");
-                console.log(res);
+                $log.log(res);
                 $rootScope.$broadcast(res);
-            }
-            else{
-                $scope.err_txt = '用户名密码错误'
+            } else {
+                $scope.errTxt = '用户名密码错误'
 
                 if (res.txt) 
-                    $scope.err_txt = res.txt
+                    $scope.errTxt = res.txt
 
                 $scope.checklogin = true;
-
 
                 $rootScope.$broadcast("login failed");
                 // showAlert();
             }
-            
         },function(){
             $rootScope.$broadcast("transimit failed wuwuwu");
         });
     }
-
-    // var showAlert = function() {
-    //    var alertPopup = $ionicPopup.alert({
-    //      template: '用户名或密码错误'
-    //    });
-    //    alertPopup.then(function(res) {
-    //      console.log('Thank you for not eating my delicious ice cream cone');
-    //    });
-    //  };
 
     $scope.logout = function(){
         AuthService.logout();
